45c3b807f479516ade2d552be9e276fdc060a470
Denys Rocha <denys.rocha@getrak.com.br>
2021-02-10 10:34:23 -0300

    Adicionando cores e temas

--- dist/styledictionary/js/tokens.js
+++ dist/styledictionary/js/tokens.js
@@ -1,17 +1,47 @@
 /**
  * Do not edit directly
- * Generated on Tue, 09 Feb 2021 12:39:36 GMT
+ * Generated on Wed, 10 Feb 2021 13:33:17 GMT
  */
 
-export const GdsPurple = "#751068";
-export const GdsDark = "#212529";
-export const GdsLight = "#f8f9fa";
-export const GdsInfo = "#0dcaf0";
-export const GdsDanger = "#dc3545";
-export const GdsWarning = "#ffc107";
-export const GdsSuccess = "#198754";
-export const GdsSecondary = "#6c757d";
-export const GdsPrimary = "#0d6efd";
+export const GdsTemaAzul = "#007fff"; // Cor para ações primárias
+export const GdsTemaNeutro = "#ededf1";
+export const GdsTemaVerde = "#34b37e"; // Cor usada para ações que indicam sucesso, disponibilidade ou acessibilidade de r
+export const GdsTemaAmarelo = "#ffaa00"; // Cor usada para avisos
+export const GdsTemaVermelho = "#c02500"; // Cor usada para ações ou feedbacks de erro
+export const GdsTemaRoxo = "#7d4ce4"; // Cor usada para ajuda, informação ou status
+export const GdsRoxoDesbotado = "#eae6ff";
+export const GdsRoxoPastel = "#bfb6f3";
+export const GdsAzulPastel = "#b2d5ff";
+export const GdsRoxoVivo = "#6457c0";
+export const GdsAzulCobalto = "#0249a6";
+export const GdsAzulRoyal = "#4a9bff";
+export const GdsVerdeAgua = "#78f1c0";
+export const GdsVerdeDesbotado = "#e3fcef";
+export const GdsAmareloDesbotado = "#fff9ed";
+export const GdsVerdeEsmeralda = "#006545";
+export const GdsAmareloCastanho = "#ffcb64";
+export const GdsAmareloMedio = "#ffecc5";
+export const GdsLaranjaMedio = "#ff8f73";
+export const GdsLaranjaDesbotado = "#ffebe6";
+export const GdsLaranjaAvermelhado = "#ff5630";
+export const GdsGelo = "#f9f9fb";
+export const GdsAzulLuz = "#ebecff";
+export const GdsCinzaClaro = "#ededf1";
+export const GdsAzulMagenta = "#b6baf1";
+export const GdsCinza = "#b3bac5";
+export const GdsAzulMistico = "#656a9c";
+export const GdsAzulLavanda = "#8c91c5";
+export const GdsAzulNoite = "#303966";
+export const GdsVioleta = "#7d4ce4";
+export const GdsAzulPetroleo = "#000125";
+export const GdsAzulAqua = "#00b8d9";
+export const GdsCiano = "#17e9e1";
+export const GdsAmareloBeer = "#ffaa00";
+export const GdsVerdeCiano = "#34b37e";
+export const GdsBrancoBase = "#ffffff";
+export const GdsVermelhoAlaranjado = "#c02500";
+export const GdsAzulEscuroBase = "#17224d";
+export const GdsAzulBrilhanteBase = "#007fff";
 export const GdsH5WebTextoMobileFamily = "Helvetica";
 export const GdsH5WebTextoMobileWeight = "Regular";
 export const GdsLegendaFamily = "Helvetica";
