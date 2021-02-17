a51a0bac55017d2a595f35f122b9f337bd8cfadf
Denys Rocha <denys.rocha@getrak.com.br>
2021-02-17 11:13:41 -0300

    retirando imagem do botao azul

--- dist/styledictionary/js/tokens.js
+++ dist/styledictionary/js/tokens.js
@@ -1,6 +1,6 @@
 /**
  * Do not edit directly
- * Generated on Wed, 17 Feb 2021 13:56:39 GMT
+ * Generated on Wed, 17 Feb 2021 14:13:15 GMT
  */
 
 export const GdsTemaAzul = "#007fff"; // Cor para ações primárias
@@ -42,36 +42,3 @@ export const GdsBrancoBase = "#ffffff";
 export const GdsVermelhoAlaranjado = "#c02500";
 export const GdsAzulEscuroBase = "#17224d";
 export const GdsAzulBrilhanteBase = "#007fff";
-export const GdsH5WebTextoMobileFamily = "Helvetica";
-export const GdsH5WebTextoMobileWeight = "Regular";
-export const GdsLegendaFamily = "Helvetica";
-export const GdsLegendaWeight = "Regular";
-export const GdsH4WebFamily = "Helvetica";
-export const GdsH4WebWeight = "Regular";
-export const GdsH3WebH4MobileFamily = "Helvetica";
-export const GdsH3WebH4MobileWeight = "Regular";
-export const GdsH2MobileFamily = "Helvetica";
-export const GdsH2MobileWeight = "Regular";
-export const GdsTextoWebTexto2MobileFamily = "Helvetica";
-export const GdsTextoWebTexto2MobileWeight = "Regular";
-export const GdsH5MobileFamily = "Helvetica";
-export const GdsH5MobileWeight = "Regular";
-export const GdsH2WebH3MobileFamily = "Helvetica";
-export const GdsH2WebH3MobileWeight = "Regular";
-export const GdsH1MobileFamily = "Helvetica";
-export const GdsH1MobileWeight = "Light";
-export const GdsTexto2WebFamily = "Helvetica";
-export const GdsTexto2WebWeight = "Regular";
-export const GdsH1WebFamily = "Helvetica";
-export const GdsH1WebWeight = "Light";
-export const GdsH5WebTextoMobileSize = "18px";
-export const GdsLegendaSize = "12.640000343322754px";
-export const GdsH4WebSize = "20.299999237060547px";
-export const GdsH3WebH4MobileSize = "22.780000686645508px";
-export const GdsH2MobileSize = "28.829999923706055px";
-export const GdsTextoWebTexto2MobileSize = "16px";
-export const GdsH5MobileSize = "20.25px";
-export const GdsH2WebH3MobileSize = "25.6299991607666px";
-export const GdsH1MobileSize = "32.439998626708984px";
-export const GdsTexto2WebSize = "14.220000267028809px";
-export const GdsH1WebSize = "28.829999923706055px";
