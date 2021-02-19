314a3fc1acdbd299da065a3d1a9891e656c5e760
Denys Rocha <denys.rocha@getrak.com.br>
2021-02-19 11:18:00 -0300

    Atualizando imagem layout xd

--- dist/styledictionary/js/tokens.js
+++ dist/styledictionary/js/tokens.js
@@ -1,6 +1,6 @@
 /**
  * Do not edit directly
- * Generated on Fri, 19 Feb 2021 12:56:39 GMT
+ * Generated on Fri, 19 Feb 2021 14:15:26 GMT
  */
 
 export const GdsTemaAzul = "#007fff"; // Cor para ações primárias
